c4c0ea2de5d13a5774f21805db9f9224c112b945
unknown <swidodo87@gmail.com>
2026-05-01 16:16:41 +0800

    Initial commit tanpa env

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\python-projects\Belajar Django 4.2. LTS\env
+set VIRTUAL_ENV=C:\python-projects\django4lts\env
 
 if not defined PROMPT set PROMPT=$P$G
 
